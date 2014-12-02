04546bdb264f2e67fa1963910e349496c99676fb
zaeem <zaeem.asif@gmail.com>
2014-12-02 15:46:44 +0500

    show image

--- app/controllers/proxies_controller.rb
+++ app/controllers/proxies_controller.rb
@@ -14,7 +14,6 @@ class ProxiesController < ApplicationController
 		@image_name = "#{@width}*#{@height}-" + @image_name.gsub!('/','_').split('?')[0]
 		@filepath = search_files
 		if @filepath.nil?
-      logger.info("---------------------------------file path nil-------------------------------------------------------")
 			image = Magick::ImageList.new  
 			urlimage = open(@url)
 			img = image.from_blob(urlimage.read)
@@ -22,11 +21,9 @@ class ProxiesController < ApplicationController
 			@filepath = "#{Rails.root}/public/resized/"+@image_name
 			resized.write(@filepath)
 		end
-
 		set_file_path
-    # save_image
-
   end
+  
   def search_files
   	filepath = nil
 		files = Dir.glob("#{Rails.root}/public/resized/*")
@@ -43,18 +40,8 @@ class ProxiesController < ApplicationController
   	end
   	filepath
   end
+
   def set_file_path
   	@file_path = url_for.split('proxies')[0] + @filepath.split('public/')[1]
   end
-  # def save_image
-  #   File file = new File(@url);
-  #   FileInputStream fis = new FileInputStream(file);
-  #   PreparedStatement ps = conn.prepareStatement("INSERT INTO images VALUES (?, ?)");
-  #   ps.setString(1, file.getName());
-  #   ps.setBinaryStream(2, fis, file.length());
-  #   ps.executeUpdate();
-  #   ps.close();
-  #   fis.close();
-  # end
-
-end
+ end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202083551) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "images", force: true do |t|
-    t.text     "image_name"
-    t.binary   "image"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
