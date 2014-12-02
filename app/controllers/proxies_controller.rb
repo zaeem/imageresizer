ce5b9991ebfa6c2072223458b7f43032b743cbf4
zaeem <zaeem.asif@gmail.com>
2014-12-02 16:14:42 +0500

    show image

--- app/controllers/proxies_controller.rb
+++ app/controllers/proxies_controller.rb
@@ -4,6 +4,9 @@ class ProxiesController < ApplicationController
 		require 'RMagick'
 		require "open-uri"
 		@url = params[:url]
+    unless params[:width] && params[:height]
+      render text: @url
+    end
 		@width = params[:width]
 		@height = params[:height]
 		if @url.index('//').present?
@@ -12,8 +15,9 @@ class ProxiesController < ApplicationController
 			@image_name = @url
 		end
 		@image_name = "#{@width}*#{@height}-" + @image_name.gsub!('/','_').split('?')[0]
-		@filepath = search_files
-		if @filepath.nil?
+    @filepath = "#{Rails.root}/public/resized/"+@image_name
+    if !File.exist?(@filepath)
+
 			image = Magick::ImageList.new  
 			urlimage = open(@url)
 			img = image.from_blob(urlimage.read)
@@ -25,24 +29,8 @@ class ProxiesController < ApplicationController
     render text: set_file_path
   end
 
-  def search_files
-  	filepath = nil
-		files = Dir.glob("#{Rails.root}/public/resized/*")
-
-  	files.each do |f|
-
-			u = f.split('/') 
-
-			if u.index(@image_name) 
-
-				filepath = f
-
-			end 
-  	end
-  	filepath
-  end
-
+  
   def set_file_path
   	@file_path = url_for.split('proxies')[0] + @filepath.split('public/')[1]
   end
- end
+end
