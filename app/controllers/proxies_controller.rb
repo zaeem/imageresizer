833b36038612a92018eae4c63504687ecfa6bbbf
zaeem <zaeem.asif@gmail.com>
2014-12-02 12:40:29 +0500

    image resizing

--- app/controllers/proxies_controller.rb
+++ app/controllers/proxies_controller.rb
@@ -1,28 +1,28 @@
 class ProxiesController < ApplicationController
   def index
-		# require 'rubygems'
-		# require 'RMagick'
-		# require "open-uri"
+		require 'rubygems'
+		require 'RMagick'
+		require "open-uri"
 
-		# @url = params[:url]
-		# @width = params[:width]
-		# @height = params[:height]
-		# if @url.index('//').present?
-		# 	@image_name = @url.split('//')[1]
-		# else
-		# 	@image_name = @url
-		# end
-		# @image_name = "#{@width}*#{@height}-" + @image_name.gsub!('/','_').split('?')[0]
-		# @filepath = search_files
-		# if @filepath.nil?
-		# 	image = Magick::ImageList.new  
-		# 	urlimage = open(@url)
-		# 	img = image.from_blob(urlimage.read)
-		# 	resized = image.resize_to_fit(@width, @height)
-		# 	@filepath = "#{Rails.root}/public/resized/"+@image_name
-		# 	resized.write(@filepath)
-		# end
-		# set_file_path
+		@url = params[:url]
+		@width = params[:width]
+		@height = params[:height]
+		if @url.index('//').present?
+			@image_name = @url.split('//')[1]
+		else
+			@image_name = @url
+		end
+		@image_name = "#{@width}*#{@height}-" + @image_name.gsub!('/','_').split('?')[0]
+		@filepath = search_files
+		if @filepath.nil?
+			image = Magick::ImageList.new  
+			urlimage = open(@url)
+			img = image.from_blob(urlimage.read)
+			resized = image.resize_to_fit(@width, @height)
+			@filepath = "#{Rails.root}/public/resized/"+@image_name
+			resized.write(@filepath)
+		end
+		set_file_path
 
   end
   def search_files
